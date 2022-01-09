d3741b0704223cab59c81fcc10914884dd590800
tsumirai <tsumirai@163.com>
2022-01-09 11:58:02 +0800

    [modify] 修改返回结构体

--- src/common/base_controller.go
+++ src/common/base_controller.go
@@ -2,12 +2,20 @@ package common
 
 import (
 	"MyServer/src/database"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
 type BaseController struct {
+}
+
+type EchoResult struct {
+	ErrNo  int         `json:"err_no"`
+	ErrMsg string      `json:"err_msg"`
+	Data   interface{} `json:"data"`
 }
 
 func (c *BaseController) GetDB() *gorm.DB {
@@ -15,9 +23,30 @@ func (c *BaseController) GetDB() *gorm.DB {
 }
 
 func (c *BaseController) EchoErrorStruct(ctx *gin.Context, errorStruct *BaseError) {
-	ctx.JSON(errorStruct.ErrNo, errorStruct.ErrMsg)
+	result := EchoResult{
+		ErrNo:  errorStruct.ErrNo,
+		ErrMsg: errorStruct.ErrMsg,
+		Data:   "",
+	}
+
+	echoJson, err := json.Marshal(result)
+	if err != nil {
+		log.Errorf("EchoErrorStruct Marshal Json Failed: %v", err.Error())
+		echoJson = make([]byte, 0)
+	}
+	ctx.JSON(errorStruct.ErrNo, echoJson)
 }
 
-func (c *BaseController) EchoSuccess(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "")
+func (c *BaseController) EchoSuccess(ctx *gin.Context, data interface{}) {
+	result := EchoResult{
+		ErrNo:  http.StatusOK,
+		ErrMsg: "",
+		Data:   data,
+	}
+	echoJson, err := json.Marshal(result)
+	if err != nil {
+		log.Errorf("EchoErrorStruct Marshal Json Failed: %v", err.Error())
+		echoJson = make([]byte, 0)
+	}
+	ctx.JSON(http.StatusOK, echoJson)
 }
